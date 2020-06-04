056f00de768aa6947b87596224bc5794532dac72
Alexey Soshin <alexey.soshin@gmail.com>
2020-06-04 14:56:32 +0100

    Remove fields method

--- kgraphql/src/main/kotlin/com/apurebase/kgraphql/schema/model/ast/DocumentNode.kt
+++ kgraphql/src/main/kotlin/com/apurebase/kgraphql/schema/model/ast/DocumentNode.kt
@@ -1,47 +1,6 @@
 package com.apurebase.kgraphql.schema.model.ast
 
-import java.util.*
-
 data class DocumentNode(
     val loc: Location?,
     val definitions: List<DefinitionNode>
-
-) {
-    val fields: List<String> by lazy {
-        val q: Queue<SelectionSetNode> = LinkedList<SelectionSetNode>()
-        val defs = this.definitions.mapNotNull { d: DefinitionNode ->
-            when (d) {
-                is DefinitionNode.ExecutableDefinitionNode ->
-                    d
-                else -> null
-            }
-        }
-        defs.forEach { d ->
-            q.add(d.selectionSet)
-        }
-
-        val fields = mutableListOf<NameNode>()
-        while (!q.isEmpty()) {
-            val curr = q.remove()
-
-            for (sel in curr.selections) {
-                when (sel) {
-                    is SelectionNode.FieldNode -> {
-                        // Has nested fields, dig deeper
-                        if (sel.selectionSet != null) {
-                            q.add(sel.selectionSet)
-                        }
-                        // This is a leaf, add to fields
-                        else {
-                            fields.add(sel.aliasOrName)
-                        }
-                    }
-                }
-            }
-        }
-
-        fields.map {
-            it.value
-        }
-    }
-}
+)
