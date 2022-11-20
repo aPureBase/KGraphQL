def5c242c97d17ab8d68f7d14676f9d1008d6713
David Kurzica <DavidKurzica18@gmail.com>
2022-11-20 02:30:35 +0100

    bump

--- kgraphql/build.gradle.kts
+++ kgraphql/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     base
-    kotlin("jvm") version "1.7.10"
-    id("org.jetbrains.dokka") version "1.7.10"
+    kotlin("jvm") version "1.7.20"
+    id("org.jetbrains.dokka") version "1.7.20"
     signing
 }
 
